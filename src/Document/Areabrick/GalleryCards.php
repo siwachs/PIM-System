eefbe12e704cae7b44fcc6f9665db39e3e906600
siwachs <ssiwach.8888@gmail.com>
2023-11-11 21:24:51 +0530

    Card Arearbrick

---
+++ src/Document/Areabrick/GalleryCards.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Document\Areabrick;
+
+class GalleryCards extends AbstractAreabrick
+{
+    public function getName(): string
+    {
+        return 'Gallery Cards';
+    }
+}
--- src/Twig/Extension/UniqidExtension.php
+++ src/Twig/Extension/UniqidExtension.php
@@ -9,7 +9,7 @@ class UniqidExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new \Twig\TwigFunction("Uniqid", function (string $prefix = '', bool $moreEntropy = false) {
+            new \Twig\TwigFunction("uniqid", function (string $prefix = '', bool $moreEntropy = false) {
                 return uniqid($prefix, $moreEntropy);
             }),
         ];
