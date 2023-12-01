88b4277ed8ebb0e4beaeb7bb8f3703916c83165b
siwachs <ssiwach.8888@gmail.com>
2023-12-02 01:21:27 +0530

    Final code

---
+++ src/EventSubscriber/DataImportSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Pimcore\Bundle\DataImporterBundle\Event\DataObject\PreSaveEvent;
+use Pimcore\Bundle\DataImporterBundle\Event\DataObject\PostSaveEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+use Pimcore\Model\Notification\Service\NotificationService;
+
+class DataImportSubscriber implements EventSubscriberInterface
+{
+    private $notificationService;
+    private $sender;
+    private $receiver;
+
+    public function __construct(NotificationService $notificationService)
+    {
+        $this->notificationService = $notificationService;
+        $this->sender = 2;
+        $this->receiver = 13;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            PreSaveEvent::class => 'onPreSave',
+            PostSaveEvent::class => 'onPostSave',
+        ];
+    }
+
+    public function onPreSave(PreSaveEvent $event)
+    {
+        // For extra use.
+    }
+
+    public function onPostSave(PostSaveEvent $event)
+    {
+        $dataObject = $event->getDataObject();
+
+        $this->sendNotification($dataObject);
+    }
+
+    private function sendNotification($dataObject)
+    {
+        $title = 'Using Class ' . $dataObject->getClassName();
+        $message = 'Data Importing finished.';
+
+        $this->notificationService->sendToUser($this->receiver, $this->sender, $title, $message);
+    }
+}
