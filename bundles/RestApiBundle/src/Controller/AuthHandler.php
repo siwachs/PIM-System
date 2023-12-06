d1b7f2dcc4d1b6672c2a2e6f49e57a27c96c1675
siwachs <ssiwach.8888@gmail.com>
2023-12-06 14:57:00 +0530

    refactored

--- bundles/RestApiBundle/src/Controller/AuthHandler.php
+++ bundles/RestApiBundle/src/Controller/AuthHandler.php
@@ -2,24 +2,16 @@
 
 namespace RestApiBundle\Controller;
 
-use Pimcore\Db;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Pimcore\Model\User as PimcoreUser;
-use Pimcore\Security\User\User as PimcoreSecurityUser;
-use Pimcore\Security\Hasher\PimcoreUserPasswordHasher;
-
 
 class AuthHandler
 {
-    private $db;
-
     public function __construct()
     {
-        $this->db = Db::getConnection();
     }
 
     /**
@@ -28,47 +20,11 @@ class AuthHandler
     public function getTokenAction(Request $request): JsonResponse
     {
         try {
-            // $data = json_decode($request->getContent(), true);
-            // $email = $data['email'];
-            // $password = $data['password'];
-            $sql = "SELECT name AS email, password FROM users WHERE type = 'user' AND name = :email";
-            // $user = $this->db->fetchAssociative($sql, ['email' => $email]);
-
-            $username = 'admin';
-            $pimcoreUserModel = PimcoreUser::getByName($username);
-
-            // Check if the user model exists and then wrap it with the Pimcore\Security\User\User class
-            if ($pimcoreUserModel instanceof PimcoreUser) {
-                // Wrap the user model with the Pimcore\Security\User\User class
-                $pimcoreSecurityUser = new PimcoreSecurityUser($pimcoreUserModel);
-
-                // Now you can access the methods and properties defined in the Pimcore\Security\User\User class
-                // For example:
-                $userId = $pimcoreSecurityUser->getId(); // Get user ID
-                $userRoles = $pimcoreSecurityUser->getRoles(); // Get user roles
-                // dd($pimcoreSecurityUser->getPassword());
-
-                $passwordHasher = new PimcoreUserPasswordHasher();
-                dd($passwordHasher->verify($pimcoreSecurityUser->getPassword(), 'admin'));
-
-
-                // ... other operations with $pimcoreSecurityUser
-            } else {
-                // Handle the case when the user with the specified name doesn't exist
-                // Example: Log an error, return an error response, etc.
-            }
-
-            return new JsonResponse([
-                // 'email' => $pimcoreSecurityUser->getPassword(),
-                'password' => null,
-                'success' => true,
-                'error' => null
-            ]);
         } catch (\Exception $e) {
             return new JsonResponse([
-                'token' => $e->getMessage(),
+                'token' => null,
                 'success' => false,
-                'error' => 'There is an error in fetching token.'
+                'error' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
