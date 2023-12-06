ec183c8973da5a70078e281ad8eae6758815c385
siwachs <ssiwach.8888@gmail.com>
2023-12-06 15:47:14 +0530

    refactored

--- bundles/RestApiBundle/src/Controller/AuthHandler.php
+++ bundles/RestApiBundle/src/Controller/AuthHandler.php
@@ -2,24 +2,26 @@
 
 namespace RestApiBundle\Controller;
 
+use Pimcore\Model\User;
+use Pimcore\Tool\Authentication;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class AuthHandler
 {
-    public function __construct()
-    {
-    }
-
     /**
      * @Route("/get-token", name="getToken", methods={"GET"})
      */
     public function getTokenAction(Request $request): JsonResponse
     {
         try {
+            $credentials = json_decode($request->getContent(), true);
+            $user = User::getByName("Mohit");
+            $data = new Authentication();
+            $res = $data->verifyPassword($user, "1234567s890");
         } catch (\Exception $e) {
             return new JsonResponse([
                 'token' => null,
